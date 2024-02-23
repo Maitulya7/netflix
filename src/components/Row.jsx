f04e444cf6e24ebb65da6ac17b7f533bcd412a9c
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-02-23 23:30:59 +0530

    #style

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Row = ({ title, fetchURL }) => {
   return (
     <>
-      <h1 className="text-white font-bold md:text-xl">{title}</h1>
+      <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
     </>
   );
 };
